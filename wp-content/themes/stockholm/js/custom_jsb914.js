780616f2980609e277f9cef4047415db1c5bfc1f
Sergio Hernández <sergio.hernandez@tec.mx>
2025-09-28 16:40:06 -0600

    primera version

---
+++ wp-content/themes/stockholm/js/custom_jsb914.js
@@ -0,0 +1,17 @@
+
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
+	"use strict";
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-126374016-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-126374016-1');
+  gtag('config', 'G-PST8N01CPP');
+</script>
+});
